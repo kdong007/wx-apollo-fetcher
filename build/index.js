1365662eae42d563a26f0494e0d4f337122d61fd
Mars Dong <kdong@hetao.ai>
2018-10-02 23:08:10 +0800

    fix presets

--- build/index.js
+++ build/index.js
@@ -1,16 +1,30 @@
+"use strict";
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = wxApolloFetcher;
+function wxApolloFetcher(url, _ref) {
+    var body = _ref.body,
+        method = _ref.method,
+        headers = _ref.headers;
 
-export default function wxApolloFetcher(url, { body, method, headers }) {
-    return new Promise((resolve, reject) => wx.request({
-        url,
-        header: headers,
-        method,
-        data: body,
-        dataType: "text",
-        success: ({ data }) => resolve({
-            text: () => Promise.resolve(data)
-        }),
-        fail: reject
-    }));
+    return new Promise(function (resolve, reject) {
+        return wx.request({
+            url: url,
+            header: headers,
+            method: method,
+            data: body,
+            dataType: "text",
+            success: function success(_ref2) {
+                var data = _ref2.data;
+                return resolve({
+                    text: function text() {
+                        return Promise.resolve(data);
+                    }
+                });
+            },
+            fail: reject
+        });
+    });
 }
-//# sourceMappingURL=index.js.map
